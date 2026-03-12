26ba6acffb5fea2f48836cb720cedf36bc6aaea9
hapybeing <gaurangk.inbox@gmail.com>
2026-03-12 17:55:37 +0530

    Create use-mouse.ts

---
+++ hooks/use-mouse.ts
@@ -0,0 +1,22 @@
+"use client"
+
+import { useEffect, useState } from "react"
+
+export function useMouse() {
+  const [position, setPosition] = useState({ x: 0, y: 0 })
+
+  useEffect(() => {
+    const handleMove = (e: MouseEvent) => {
+      setPosition({
+        x: e.clientX,
+        y: e.clientY
+      })
+    }
+
+    window.addEventListener("mousemove", handleMove)
+
+    return () => window.removeEventListener("mousemove", handleMove)
+  }, [])
+
+  return position
+}
